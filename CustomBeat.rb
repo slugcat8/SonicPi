77e53bebc93c2bbe0ebaab06b82469ef53fe614c
slugcat8 <99358691+slugcat8@users.noreply.github.com>
2022-04-19 11:00:31 -0700

    Update CustomBeat.rb

--- CustomBeat.rb
+++ CustomBeat.rb
@@ -1,31 +1,39 @@
 # Change user in file name to "papap"
+
 deep_light1 = "C:/Users/micah_sessions/Downloads/Rain_World_OST_-_Deep_Light_(getmp3.pro).wav"
 deep_light2 = "C:/Users/micah_sessions/Downloads/Rain_World_OST_-_Deep_Light2_(getmp3.pro).wav"
 
-
-live_loop :main1 do
-  sample :ambi_piano, amp: 2, beat_stretch: 3
-  sleep 3
+define :ambi_piano do |a, b, s|
+  sample :ambi_piano, amp: a, beat_stretch: b
+  sleep s
 end
 
-live_loop :main2 do
+define :bd_boom do
   sample :bd_boom, amp: 2
   sleep 3
 end
 
+define :piano_sting do
+  sample deep_light1, amp: 2
+  ambi_piano 3, 7, 9
+  sample deep_light2, amp: 2
+  ambi_piano 3, 7, 9
+end
+
+#<><>
+
+live_loop :ambi_piano_main_loop do
+  ambi_piano 2, 3, 3
+end
+
+live_loop :bd_boom_loop do
+  bd_boom
+end
+
 sleep 6
 
-dl = 1
-live_loop :main4 do
-  if dl = 2
-    sample deep_light1, amp: 2
-    dl = 1
-  else
-    sample deep_light2, amp: 2
-    dl = 2
-  end
-  sample :ambi_piano, amp: 3, beat_stretch: 7
-  sleep 9
+live_loop :piano_sting_loop do
+  piano_sting
 end
 
 sleep 9
