1677483422697bfe569bf26a11a7a4d24f394b62
slugcat8 <99358691+slugcat8@users.noreply.github.com>
2022-04-27 19:46:21 -0700

    Update AP CS Performance Task.rb

--- AP CS Performance Task.rb
+++ AP CS Performance Task.rb
@@ -1,6 +1,6 @@
 # AP Computer Science Principles Performance Task
 # all externl samples used are from "Titan" by Disasterpeace (https://www.youtube.com/watch?v=D6os0Upx5A0)
-# the rythmn of the notes in synth_main1 and drum_main2 are inspired by Waveshaper's "A Picture in Motion" (https://www.youtube.com/watch?v=ZUlAytznxn4)
+# the rythmn of the notes in synth_main, synth_alt, and drum_main2 are inspired by Waveshaper's "A Picture in Motion" (https://www.youtube.com/watch?v=ZUlAytznxn4)
 
 use_bpm 90
 use_synth :saw
@@ -8,9 +8,43 @@ use_synth :saw
 titan1 = "C:/Users/papap/Downloads/titan_sample1.wav"
 titan2 = "C:/Users/papap/Downloads/titan_sample2.wav"
 titan3 = "C:/Users/papap/Downloads/titan_sample3.wav"
+titan4 = "C:/Users/papap/Downloads/titan_sample4.wav"
+titan5 = "C:/Users/papap/Downloads/titan_sample5.wav"
 titan_final = "C:/Users/papap/Downloads/titan_sample_final.wav"
 
-define :synth_main1 do |n1, n2, n3, n4, n5, n6, n7, n8, synth_amp|
+define :synth_main do |v, synth_amp|
+  with_fx :ping_pong do
+    if v == 0
+      play :a, amp: synth_amp
+      play :a4, amp: synth_amp
+      sleep 0.5
+      play :d4, amp: synth_amp
+      play :a, amp: synth_amp
+      sleep 0.5
+      play :e4, amp: synth_amp
+      play :d4, amp: synth_amp
+      sleep 0.5
+      play :a4, amp: synth_amp
+      play :e4, amp: synth_amp
+      sleep 0.5
+    else
+      play :f, amp: synth_amp
+      play :a4, amp: synth_amp
+      sleep 0.5
+      play :d4, amp: synth_amp
+      play :f, amp: synth_amp
+      sleep 0.5
+      play :e4, amp: synth_amp
+      play :d4, amp: synth_amp
+      sleep 0.5
+      play :a4, amp: synth_amp
+      play :e4, amp: synth_amp
+      sleep 0.5
+    end
+  end
+end
+
+define :synth_alt do |n1, n2, n3, n4, n5, n6, n7, n8, synth_amp|
   with_fx :ping_pong do
     play n1, amp: synth_amp
     play n2, amp: synth_amp
@@ -47,18 +81,18 @@ define :drum_main2 do
   end
 end
 
-titan_synth = [titan1, titan2, titan3]
+titan_synth = [titan1, titan2, titan3, titan4, titan5]
 
 index = 0
 titan_amp = 0.25
 define :titan_creschendo do
   sample titan_synth[index], attack: 5, rate: 2, amp: titan_amp
   index = index + 1
-  if index >= 3
+  if index >= 5
     index = 0
   end
-  titan_amp = titan_amp + 0.5
-  sleep 5
+  titan_amp = titan_amp + 0.2
+  sleep 4
 end
 
 define :titan_main do
@@ -73,15 +107,15 @@ end
 #M1
 n = 1
 odd_even = 0
-8.times do
+2.times do
   odd_even = n.modulo(2)
   if odd_even == 0
     2.times do
-      synth_main1 :f, :a4, :d4, :f, :e4, :d4, :a4, :e4, 1
+      synth_main 1, 1
     end
   else
     2.times do
-      synth_main1 :a, :a4, :d4, :a, :e4, :d4, :a4, :e4, 1
+      synth_main 0, 1
     end
   end
   n = n + 1
@@ -95,35 +129,35 @@ live_loop :drum_main do
 end
 
 2.times do
-  synth_main1 :a, :a4, :d4, :a, :e4, :d4, :a4, :e4, 1
+  synth_main 0, 1
 end
 2.times do
-  synth_main1 :f, :a4, :d4, :f, :e4, :d4, :a4, :e4, 1
+  synth_main 1, 1
 end
 dm_amp = dm_amp + 1
 2.times do
-  synth_main1 :a, :g4, :d4, :a, :e4, :d4, :g4, :e4, 1
+  synth_alt :a, :g4, :d4, :a, :e4, :d4, :g4, :e4, 1
 end
 2.times do
-  synth_main1 :f, :g4, :d4, :f, :e4, :d4, :g4, :e4, 1
+  synth_alt :f, :g4, :d4, :f, :e4, :d4, :g4, :e4, 1
 end
 dm_amp = dm_amp + 1
 2.times do
-  synth_main1 :a, :g4, :d4, :a, :e4, :d4, :g4, :e4, 1
+  synth_alt :a, :g4, :d4, :a, :e4, :d4, :g4, :e4, 1
 end
 2.times do
-  synth_main1 :fs, :g4, :d4, :fs, :e4, :d4, :g4, :e4, 1
+  synth_alt :fs, :g4, :d4, :fs, :e4, :d4, :g4, :e4, 1
 end
 
-sample :ambi_lunar_land, amp: 2
-sleep 6
+sample :ambi_lunar_land, rate: 2, amp: 2
+sleep 3
 
-3.times do
+5.times do
   titan_creschendo
 end
 
-sample titan_final, attack: 4, amp: 4
-sleep 9
+sample titan_final, rate: 2, attack: 4, amp: 4
+sleep 4
 
 titan_amp = 5
 live_loop :titan do
@@ -148,18 +182,18 @@ end
 synth_amp = 1
 live_loop :in_motion do
   2.times do
-    synth_main1 :a, :a4, :d4, :a, :e4, :d4, :a4, :e4, synth_amp
+    synth_main 0, synth_amp
   end
   2.times do
-    synth_main1 :f, :a4, :d4, :f, :e4, :d4, :a4, :e4, synth_amp
+    synth_main 1, synth_amp
   end
   2.times do
     1.times do
-      synth_main1 :a, :a4, :d4, :a, :e4, :d4, :a4, :e4, synth_amp
+      synth_main 0, synth_amp
     end
     synth_amp = synth_amp - 0.25
     1.times do
-      synth_main1 :f, :a4, :d4, :f, :e4, :d4, :a4, :e4, synth_amp
+      synth_main 1, synth_amp
     end
     synth_amp = synth_amp - 0.25
   end
